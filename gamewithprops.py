7c392770e6acb86a9dea5a789ff1a27c7c2fcf03
YvoTan <ytan0815@uni.sydney.edu.au>
2025-05-25 21:23:12 +1000

    Update gamewithprops.py

--- gamewithprops.py
+++ gamewithprops.py
@@ -7,7 +7,8 @@ class Game:
         self.round_winner = None
         self.current_player = 0
         self.boxes = {player: [] for player in self.players}
-        self.hammers = {player: False for player in self.players}  # check Hammer
+        self.hammers = {player: False for player in self.players}  # Track  Hammer
+        self.hammer_used = {player: False for player in self.players}  # hammer used
 
     def setup_boxes(self):
         for player in self.players:
@@ -26,6 +27,10 @@ class Game:
             return False
 
     def use_hammer(self, player):
+        if self.hammer_used[player]:
+            print(f"{player}, you have already used your Hammer this round.")
+            return False
+        
         print(f"{player}, you have a Hammer!")
         action = input('Do you want to use the Hammer? (y/n): ')
         if action.lower() == 'y':
@@ -33,8 +38,17 @@ class Game:
             box_index = int(input(f'Choose a box to destroy (1-5) from {self.players[target_player]}: ')) - 1
             self.boxes[self.players[target_player]][box_index] = -1  # box destroyed
             print(f'Box {box_index + 1} of {self.players[target_player]} has been destroyed!')
+            self.hammer_used[player] = True  
             return True
         return False
+
+    def get_valid_box_index(self):
+        while True:
+            action = input('Choose a box (1-5): ')
+            if action.isdigit() and 1 <= int(action) <= 5:
+                return int(action) - 1
+            else:
+                print("Invalid input. Please enter a number between 1 and 5.")
 
     def play_round(self):
         self.setup_boxes()
@@ -42,11 +56,10 @@ class Game:
 
         while max(gold_count) < 2:
             print(f"\nIt's {self.players[self.current_player]}'s turn.")
-            if self.hammers[self.players[self.current_player]]:
+            if self.hammers[self.players[self.current_player]] and not self.hammer_used[self.players[self.current_player]]:
                 self.use_hammer(self.players[self.current_player])
 
-            action = input('Choose a box (1-5): ')
-            box_index = int(action) - 1
+            box_index = self.get_valid_box_index()
 
             if self.reveal_box(self.players[self.current_player], box_index):
                 gold_count[self.current_player] += 1
@@ -64,10 +77,11 @@ class Game:
     def next_round(self):
         if self.round_winner:
             self.hammers[self.round_winner] = True
+            self.hammer_used = {player: False for player in self.players}
             loser = self.players[1 - self.current_player]
             print(f'{loser} starts the next round.')
             self.current_player = 1 - self.current_player
-            self.round_winner = None  # hammer to the winner according to previous round
+            self.round_winner = None
 
     def check_winner(self):
         for i, score in enumerate(self.scores):
